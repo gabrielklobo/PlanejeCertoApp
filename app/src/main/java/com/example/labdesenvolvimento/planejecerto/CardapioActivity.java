9bb466f4406b3e4d5c1de484027914125e25136c
Kauan Polydoro <kauan.2812@hotmail.com>
2017-09-25 21:35:11 -0300

    Initial commit

---
+++ app/src/main/java/com/example/labdesenvolvimento/planejecerto/CardapioActivity.java
@@ -0,0 +1,15 @@
+package com.example.labdesenvolvimento.planejecerto;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CardapioActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cardapio);
+    }
+}
