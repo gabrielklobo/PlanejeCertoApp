d2716d0d11c1568bda28024ef2011e5a45c16832
gabrielklobo <ga.p.lobo@gmail.com>
2017-10-02 20:35:44 -0300

    Adicionado Label

--- app/src/main/java/com/example/labdesenvolvimento/planejecerto/CardapioActivity.java
+++ app/src/main/java/com/example/labdesenvolvimento/planejecerto/CardapioActivity.java
@@ -1,7 +1,10 @@
 package com.example.labdesenvolvimento.planejecerto;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CardapioActivity extends Activity
 {
@@ -11,5 +14,23 @@ public class CardapioActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cardapio);
+
+        Button vou = (Button) findViewById(R.id.button);
+        Button nVou = (Button) findViewById(R.id.button3);
+        nVou.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CardapioActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        vou.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CardapioActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
