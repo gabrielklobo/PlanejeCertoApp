eaf29e2a806dfc091508244ec7e6618ba2001257
LexGalante <alex_gm_100@hotmail.com>
2017-11-06 21:50:46 -0200

    TESte

--- app/src/main/java/com/example/labdesenvolvimento/planejecerto/MainActivity.java
+++ app/src/main/java/com/example/labdesenvolvimento/planejecerto/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends Activity {
     static List<String> unidade = new ArrayList<>();
     static List<String> servico = new ArrayList<>();
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
